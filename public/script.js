bf2f777479fab4fe0a828c6aaa4c03980368ceab
Wendy Kan <wendykan@gmail.com>
2020-04-05 16:14:04 -0700

    adding nyt data

--- public/script.js
+++ public/script.js
@@ -19,48 +19,66 @@ firebase.initializeApp(config);
 // Those sliding arrays produce a live data effect.
 // -----
 // See https://firebase.google.com/docs/database/web/lists-of-data for trigger syntax:
-firebase.database().ref('1neJU9AKBjDqCWke0x4d7QRSA9zqoPERahumhBKgLO1k/Sheet1').on('value', ts_measures => {
-    // If you want to get into details, read the following comments :-)
-    // 'ts_measures' is a snapshot raw Object, obtained on changed value of 'timestamped_measures' node
-    // e.g. a new push to that node, but is not exploitable yet.
-    // If we apply the val() method to it, we get something to start work with,
-    // i.e. an Object with the 'nbOfElts' last nodes in 'timestamped_measures' node.
-    // console.log(ts_measures.val());
-    // => {-LIQgqG3c4MjNhJzlgsZ: {timestamp: 1532694324305, value: 714}, -LIQgrs_ejvxcF0MqFre: {…}, … }
 
-    // We prepare empty arrays to welcome timestamps and luminosity values:
+function processData(allRows,xLabel,yLabel,yTraceName) {
+
+  console.log(allRows.length);
+  var x = [], y = [];
+
+  for (var i=0; i<allRows.length; i++) {
+    row = allRows[i];
+ 
+    x.push(row[xLabel]);
+    y.push(row[yLabel]);
+    if(i === 0) {
+      console.log(i);
+      console.log(row);
+    }
+  }
+  makePlotly( x, y,  yTraceName);
+}
+
+
+
+firebase.database().ref('1neJU9AKBjDqCWke0x4d7QRSA9zqoPERahumhBKgLO1k/Sheet1').on('value', ts_measures => {
     let timestamps = [];
     let confirmed_cases = [];
+    let increase_ratio = [];
+    let positive_rate = [];
 
-    // Next, we iterate on each element of the 'ts_measures' raw Object
-    // in order to fill the arrays.
-    // Let's call 'ts_measure' ONE element of the ts_measures raw Object
-    // A handler function written here as an anonymous function with fat arrow syntax
-    // tells what to do with each element:
-    // * apply the val() method to it to gain access to values of 'timestamp' and 'value',
-    // * push those latter to the appropriate arrays.
-    // Note: The luminosity value is directly pushed to 'values' array but the timestamp,
-    // which is an Epoch time in milliseconds, is converted to human date
-    // thanks to the moment().format() function coming from the moment.js library.    
     ts_measures.forEach(ts_measure => {
-        //console.log(ts_measure.val().timestamp, ts_measure.val().value);
         timestamps.push(moment(ts_measure.val().date).format('YYYY-MM-DD HH:mm:ss'));
         confirmed_cases.push(ts_measure.val().confirmed_cases);
+        increase_ratio.push(ts_measure.val().increased_ratio);
+        positive_rate.push(ts_measure.val().positive_rate);
     });
 
-    // Get a reference to the DOM node that welcomes the plot drawn by Plotly.js:
-    myPlotDiv = document.getElementById('myPlot');
+    myPlotDiv1 = document.getElementById('myPlot1');
+    myPlotDiv2 = document.getElementById('myPlot2');
+    myPlotDiv3 = document.getElementById('myPlot3');
 
-    // We generate x and y data necessited by Plotly.js to draw the plot
-    // and its layout information as well:
-    // See https://plot.ly/javascript/getting-started/
-    const data = [{
+
+
+    const data1 = {
         x: timestamps,
-        y: confirmed_cases
+        y: confirmed_cases,
+        type: 'bar',
+        name: 'Marin HHS Cases'
+    };
+    const data2 = [{
+        x: timestamps,
+        y: increase_ratio,
+        type: 'bar'
+    }];
+    const data3 = [{
+        x: timestamps,
+        y: positive_rate,
+        type: 'bar'
     }];
 
-    const layout = {
-        title: '<b>Marin County, California COVID-19 Confirmed Cases By Date </b>',
+
+    const layout1 = {
+        title: 'Marin County, California COVID-19 Confirmed Cases By Date',
         titlefont: {
             family: 'Courier New, monospace',
             size: 16,
@@ -71,7 +89,7 @@ firebase.database().ref('1neJU9AKBjDqCWke0x4d7QRSA9zqoPERahumhBKgLO1k/Sheet1').o
             linewidth: 2
         },
         yaxis: {
-            title: '<b>10-bit value</b>',
+            title: 'Number of cases',
             titlefont: {
                 family: 'Courier New, monospace',
                 size: 14,
@@ -84,7 +102,97 @@ firebase.database().ref('1neJU9AKBjDqCWke0x4d7QRSA9zqoPERahumhBKgLO1k/Sheet1').o
             r: 50,
             pad: 0
         }
-    }
+    };
+    const layout2 = {
+        title: 'Marin County, California COVID-19 Daily Increase Ratio',
+        titlefont: {
+            family: 'Courier New, monospace',
+            size: 16,
+            color: '#000'
+        },
+        xaxis: {
+            linecolor: 'black',
+            linewidth: 2
+        },
+        yaxis: {
+            title: 'ratio',
+            titlefont: {
+                family: 'Courier New, monospace',
+                size: 14,
+                color: '#000'
+            },
+            linecolor: 'black',
+            linewidth: 2,
+        },
+        margin: {
+            r: 50,
+            pad: 0
+        }
+    };
+    const layout3 = {
+        title: 'Marin County, California COVID-19 Daily Test Positive Rate',
+        titlefont: {
+            family: 'Courier New, monospace',
+            size: 16,
+            color: '#000'
+        },
+        xaxis: {
+            linecolor: 'black',
+            linewidth: 2
+        },
+        yaxis: {
+            title: 'ratio',
+            titlefont: {
+                family: 'Courier New, monospace',
+                size: 14,
+                color: '#000'
+            },
+            linecolor: 'black',
+            linewidth: 2,
+        },
+        margin: {
+            r: 50,
+            pad: 0
+        }
+    };
+    var nyt_data_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv';
+    Plotly.d3.csv(nyt_data_url, function(err, rows){
+        var x = [], y = [], deaths=[];
+
+        for (var i=0; i<rows.length; i++) {
+            row = rows[i];
+            if(row['fips']=='06041') {
+                x.push(moment(Date.parse(row['date'])).format('YYYY-MM-DD HH:mm:ss'));
+                y.push(parseInt(row['cases']));
+                deaths.push(parseInt(row['deaths']))
+            }
+            // if(i === 0) {
+            //   console.log(i);
+            //   console.log(row);
+            // }
+        }
+        // console.log(y);
+
+        var nyt_cases = {
+            x: x,
+            y: y,
+            type: 'bar',
+            name: 'NYTimes Cases'
+        };
+        var nyt_deaths = {
+            x: x,
+            y: deaths,
+            type: 'scatter',
+            name: 'NYTimes Deaths'
+        };
+
+        traces = [data1, nyt_cases, nyt_deaths];
+        Plotly.newPlot(myPlotDiv1, traces,layout1,{ responsive: true });
+        
+    });
+
     // At last we plot data :-)
-    Plotly.newPlot(myPlotDiv, data, layout, { responsive: true });
+    // Plotly.newPlot(myPlotDiv1, data1, layout1, { responsive: true });
+    Plotly.newPlot(myPlotDiv2, data2, layout2, { responsive: true });
+    Plotly.newPlot(myPlotDiv3, data3, layout3, { responsive: true });
 });
